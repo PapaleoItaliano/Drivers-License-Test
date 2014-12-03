d99c89c6807830ac18d2e4a509a91a7cb8c6cd1f
Kevin Dinh <asc2@10.9.34.27>
2014-12-03 13:25:31 -0600

    FIX TEXT BOXES PLZ

--- Swing.Drivers-Ed/src/DriversEd_Swing/BackgroundComponents.java
+++ Swing.Drivers-Ed/src/DriversEd_Swing/BackgroundComponents.java
@@ -1,23 +1,33 @@
 package DriversEd_Swing;
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 public class BackgroundComponents extends JFrame{
 	public BackgroundComponents (String Title)
 	{
 		super(Title);
 		setLayout(new BorderLayout());
-		JTextArea textarea = new JTextArea();
+		JTextArea textarea = new JTextArea(5,10);
+		JTextField textfield = new JTextField(30);
+		JPanel jp = new JPanel();
+		jp.setLayout(new FlowLayout(FlowLayout.RIGHT));
 		JButton button = new JButton("FHRITP");
-		
-		Container c = getContentPane();
-		c.add(textarea, BorderLayout.CENTER);
-		c.add(button,BorderLayout.SOUTH);
+		jp.add(textfield);
+		add(jp);
+		getContentPane().add(jp, "Center");
+		/*Container c = getContentPane();
+		c.add(textfield);*/
+		/*c.add(button,BorderLayout.SOUTH);*/
 		button.addActionListener(new ActionListener()
 		{
 			@Override
@@ -26,6 +36,7 @@ public class BackgroundComponents extends JFrame{
 				textarea.append("ASDA\n");
 			}
 		});
+		
 		
 	}
 }
