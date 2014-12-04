c909051d364de9285d3a49c1422787254a6f156e
Kevin Dinh <asc2@10.9.34.27>
2014-12-04 13:10:41 -0600

    Still Going

--- Swing.Drivers-Ed/src/DriversEd_Swing/Main_Runner.java
+++ Swing.Drivers-Ed/src/DriversEd_Swing/Main_Runner.java
@@ -10,7 +10,7 @@ public class Main_Runner {
 		SwingUtilities.invokeLater(new Runnable() {
 
 			public void run() {
-				JFrame run = new BackgroundComponents("~~~~~Driver's Ed Runner~~~~~");
+				JFrame run = new Driver_Test("Driver_Test");
 				run.setSize(600, 400);
 				run.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				run.setLocationRelativeTo(null);
