ed1a3d70e6bbedc249b9d7e7e55f7c9c813ea39e
asc2 <asc2@10.9.34.60>
2014-12-05 12:57:24 -0600

    tuesday

---
+++ Swing.Drivers-Ed/src/DriversEd_Swing/DriversTest.java
@@ -0,0 +1,96 @@
+package DriversEd_Swing;
+
+import java.util.Scanner;
+
+public class DriversTest 
+{
+	static char[] answers = new char[20];
+	char[] test = new char[20];
+	public DriversTest(char[] temp)
+	{
+		 test = temp;
+	}
+	
+	public boolean passed()
+	{
+		int wrong = 0;
+		for(int i = 0; i < test.length; i++)
+		{
+			if(test[i] != answers[i])
+				wrong++;
+		}
+		if(wrong <= 5)
+			return true;
+		else return false;
+	}
+	
+	public int totalCorrect()
+	{
+		int right = 0;
+		for(int i = 0; i < test.length; i++)
+		{
+			if(test[i] == answers[i])
+				right++;
+		}
+		return right;
+	}
+	
+	public int totalIncorrect()
+	{
+		int wrong = 0;
+		for(int i = 0; i < test.length; i++)
+		{
+			if(test[i] != answers[i])
+				wrong++;
+		}
+		
+		return wrong;
+	}
+	
+	public int[] questionsMissed()
+	{
+		int[] wrong = new int[this.totalIncorrect()];
+		int f = 0;
+		while (f < totalIncorrect())
+		{
+			for(int i = 0; i < test.length; i++)
+			{
+				if(test[i] != answers[i])
+				{
+					wrong[f] = i;
+					f++;
+				}
+			}
+		}
+		return wrong;
+	}
+	
+	public static void changeKey()
+	{
+		char[] newKey = new char[20];
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the new answers");
+		for(int i = 0; i < 20; i++)
+		{
+			System.out.print(i + 1 + ": ");
+			String a = scan.next();
+			newKey[i] = a.charAt(0);
+		}
+		System.out.println("\nNew answer key done");
+		answers = newKey;
+	}
+	
+	public static char[] getKey()
+	{
+		return answers;
+	}
+	
+	public static void setKey(String key)
+	{
+		for(int i = 0; i < 20; i++)
+		{
+			answers[i] = key.charAt(i);
+		}
+	}
+	
+}
